0e51621fec00a0872af815643d69d578c6587201
Harsh2460 <sharshvardhan2006@gmail.com>
2026-09-13 22:58:35 +0530

    Add files via upload

---
+++ Ch1/03.comments.py
@@ -0,0 +1,11 @@
+import pyjokes
+
+# 1.Single line comments
+# This prints a random joke 
+joke = pyjokes.get_joke()
+print(joke)
+
+# 2.Multi line comments
+"""This is an amazing 
+example of a Multiline 
+comment!"""
