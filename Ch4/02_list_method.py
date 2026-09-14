f4419dad937de62037c7faa137478b96ba590dab
Harsh2460 <sharshvardhan2006@gmail.com>
2026-09-14 23:47:16 +0530

    Add files via upload

---
+++ Ch4/02_list_method.py
@@ -0,0 +1,24 @@
+friends = ["Apple", "Orange", 5,345.06, False, "Aakash", "Rohan"]
+
+print(friends)
+friends.append("Harry")
+print(friends)
+
+l1 = [1,34,62,2,6,11]
+# l1.sort()
+# l1.reverse()
+# l1.insert(3,333333) # Insert 333333 such that its index in the list is 3
+# value = l1.pop(3)
+# print(value)
+# l1.remove(6)
+# print(l1.index(34))
+# print(l1.clear())
+print(l1)
+
+l2 = [1,34,62,2,6,11,62]
+l3 = [72,80,68,46,12]
+# print(l2.count(62))
+# l2.extend(l3)
+# print(l2)
+l2.extend([4,5,6,7,8])
+print(l2)
---
+++ Ch4/04_tuple_methods.py
@@ -0,0 +1,28 @@
+# a = (1,45,342,3424,False,"Rohan",45,"Shivam")
+
+# print(a)
+
+# no = a.count(45)
+# print(no)
+
+# i = a.index(3424)
+# print(i)
+
+# Operation in Tuple
+
+
+t1 = (1,2,5,6)
+t2 = (3,4,7,8)
+
+# print(t1[1]) # acessing element(indexing)
+# print(t2[-1]) # negative indexing
+# print(t1[1:4]) # slicing
+
+# print(t1 + t2) # concatenation
+# print(t1 * 2) # repetition
+# print(5 in t1) # membership
+
+# print(len(t1)) # length
+# print(max(t1)) # maximum
+# print(min(t2)) # minimum
+# print(sum(t1)) # sum
