91bcb6812d6fe9eb363ecf600969012a2932960f
Harsh2460 <sharshvardhan2006@gmail.com>
2026-09-16 20:13:54 +0530

    Add files via upload

---
+++ Ch8/01_functions.py
@@ -0,0 +1,34 @@
+# a = int(input("Enter the number: "))
+# b = int(input("Enter the number: "))
+# c = int(input("Enter the number: "))
+
+# avg = (a + b + c)/3
+# print(avg)
+
+# a = int(input("Enter the number: "))
+# b = int(input("Enter the number: "))
+# c = int(input("Enter the number: "))
+
+# avg = (a + b + c)/3
+# print(avg)
+
+# or
+
+# Function Definitin
+def avg():
+    a = int(input("Enter the number: "))
+    b = int(input("Enter the number: "))
+    c = int(input("Enter the number: "))
+
+    avg = (a + b + c)/3
+    print(avg)
+
+# Function Call
+avg()
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+avg()
